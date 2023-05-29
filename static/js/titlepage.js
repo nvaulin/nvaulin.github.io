ab476e7529bf057726e1d66cc1fd9842c1e4aaf6
Nikita Vaulin <vaulin@ro.ru>
2023-05-29 15:53:51 +0300

    Add files via upload

---
+++ static/js/titlepage.js
@@ -0,0 +1,10 @@
+const greetElement = document.getElementById("greeting");
+const nameElement = document.getElementById("name");
+const names = ["Nikita Vaulin", "Nikita Vaulin", "Никита Ваулин"];
+const greetings = ["Hi, I am", "Salut, je m'appelle", "Привет, это я"];
+let currentIndex = 0;
+setInterval(() => {
+currentIndex = (currentIndex + 1) % greetings.length;
+greetElement.textContent = greetings[currentIndex];
+nameElement.textContent = names[currentIndex];
+}, 5000);
